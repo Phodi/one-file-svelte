9232942a2a2f28e6a4992665800d44fbedcf8a2c
Phodi <5670253+Phodi@users.noreply.github.com>
2025-01-30 10:43:06 +0700

    upgrade to svelte 5

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
-<script>
+<script lang="ts">
   import "./style.css";
-  import { onMount } from "svelte";
   import { initializeApp } from "./app_init";
-
-  onMount(() => {
+  $effect(() => {
     initializeApp();
   });
+  let count = $state(0);
 </script>
 
-<p>Hello World!</p>
-<button class="btn">OK</button>
+<p>Count = {count}</p>
+<button
+  class="btn"
+  onclick={() => {
+    count = count + 1;
+  }}>ADD</button
+>
